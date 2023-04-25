9fdbd02ab1e0d6523c6ea603df95e18e6dfe2165
Bikkul <quadalus@yandex.ru>
2023-04-25 14:46:20 +0300

    feat: намечена базовая структура user service

---
+++ mwb-user-service/src/main/java/ru/bikkul/mwb/main/MyWalletBalanceUserService.java
@@ -0,0 +1,11 @@
+package ru.bikkul.mwb.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWalletBalanceUserService {
+    public static void main(String[] args) {
+        SpringApplication.run(MyWalletBalanceUserService.class, args);
+    }
+}
---
+++ mwb-user-service/src/main/java/ru/bikkul/mwb/main/model/User.java
@@ -0,0 +1,29 @@
+package ru.bikkul.mwb.main.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String secondName;
+
+    private String  login;
+
+    private String email;
+
+    private String password;
+}
